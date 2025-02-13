c9a8617152fbe4be3b2ebdc166578de4549cc059
tinttituulia <tuulia.tolonen@edu.sasky.fi>
2025-02-13 11:31:23 +0200

    lisää sovelluksen testidatan

---
+++ src/components/App/testdata.js
@@ -0,0 +1,59 @@
+const testdata = [
+    {
+      id:          "1",
+      type:        "Sähkö",
+      amount:      437.50,
+      paymentDate: "2023-03-20",
+      periodStart: "2022-12-01",
+      periodEnd:   "2023-02-28",
+      receiver:    "Caruna Oy"
+    },
+    {
+      id:          "2",
+      type:        "Puhelin",
+      amount:      26.90,
+      paymentDate: "2023-03-18",
+      periodStart: "",
+      periodEnd:   "",
+      receiver:    "Elisa"
+    },
+    {
+      id:          "3",
+      type:        "Sähkö",
+      amount:      87.90,
+      paymentDate: "2023-02-28",
+      periodStart: "",
+      periodEnd:   "",
+      receiver:    "Helen"
+    },  
+    {
+      id:          "4",
+      type:        "Vesi",
+      amount:      352.00,
+      paymentDate: "2023-02-17",
+      periodStart: "2022-09-01",
+      periodEnd:   "2022-12-31",
+      receiver:    "Sastamalan vesilaitos"
+    },
+    {
+      id:          "5",
+      type:        "Puhelin",
+      amount:      26.90,
+      paymentDate: "2023-02-17",
+      periodStart: "",
+      periodEnd:   "",
+      receiver:    "Elisa"
+    },
+    {
+      id:          "6",
+      type:        "Vero",
+      amount:      178.54,
+      paymentDate: "2023-02-16",
+      periodStart: "2023-01-01",
+      periodEnd:   "2023-12-31",
+      receiver:    "Verovirasto"
+    } 
+  ]
+  
+  export default testdata
+  
