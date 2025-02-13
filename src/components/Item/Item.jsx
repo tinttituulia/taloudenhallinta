184dd57d1c314f7db269f974ee8e14452df0080f
tinttituulia <tuulia.tolonen@edu.sasky.fi>
2025-02-13 11:50:05 +0200

    lisää valuuttamuotoilun Item-komponentille

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -6,13 +6,15 @@ function Item({data, ...props}) {
 
   const locale = "fi-FI"
   const paymentDate = new Date(data.paymentDate).toLocaleDateString(locale)
+  const numberFormat = new Intl.NumberFormat(locale, { style: 'currency', currency: 'EUR' })
+  const amount = numberFormat.format(data.amount)
 
 
   return (
     <div className={styles.item}>
         <div className={styles.item_data}>
         <div className={styles.item_type}>{data.type}</div>
-        <div className={styles.item_amount}>{data.amount} €</div>
+        <div className={styles.item_amount}>{amount}</div>
         <div className={styles.item_date}>{paymentDate}</div>
         <div className={styles.item_timespan}>{data.periodStart} - {data.periodEnd}</div>
         <div className={styles.item_receiver}>{data.receiver}</div>
