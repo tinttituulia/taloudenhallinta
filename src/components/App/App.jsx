9979a754dba85a0011caed191a6f01485ca131be
tinttituulia <tuulia.tolonen@edu.sasky.fi>
2025-02-18 13:29:30 +0200

    lisää muokatun merkinnän korvaamisen

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,7 +8,13 @@ function App() {
 const [data, setData] = useState(testdata)
 const handleItemSubmit = (newitem) => {
   let copy = data.slice()
-  copy.push(newitem)
+  const index = copy.findIndex(item => item.id === newitem.id)
+  if (index >= 0) {
+    copy[index] = newitem
+  } else {
+    copy.push(newitem)
+  }
+
   copy.sort( (a,b) => {
     const aDate = new Date(a.paymentDate)
     const bDate = new Date(b.paymentDate)
