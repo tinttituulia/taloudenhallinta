7905a462063722c6b1e24347281d0ce308cd29fb
tinttituulia <tuulia.tolonen@edu.sasky.fi>
2025-02-19 16:37:41 +0200

    lisää kulutyyppilistan tulostuksen

--- src/components/AppRouter/AppRouter.jsx
+++ src/components/AppRouter/AppRouter.jsx
@@ -34,7 +34,8 @@ function AppRouter(props) {
             }
           } },
           { path: "stats", element: <Stats /> },
-          { path: "settings", element: <Settings /> },
+          { path: "settings", 
+            element: <Settings typelist={props.typelist} /> }  
       ]
     }
   ])
--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,10 +1,16 @@
 import styles from './Settings.module.scss'
 
-function Settings() {
+function Settings(props) {
 
   return (
     <div className={styles.settings}>
-      <h2>Settings</h2>
+      <h2>Asetukset</h2>
+      <h3>Kulutyypit</h3>
+      <div className={styles.settings_types}>
+        { props.typelist.map(
+            type => <div key={type}>{type}</div>
+        )}
+      </div>
     </div>
   )
 }
