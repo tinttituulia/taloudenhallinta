13d1693a2b89279e16b2fe8345eeab33b18ca3eb
tinttituulia <tuulia.tolonen@edu.sasky.fi>
2025-11-04 10:25:44 +0200

    lisää PWA-lisäosan ja sen määritykset

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa"
+import manifest from './manifest.json'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({ manifest: manifest })],
   test: {
     globals: true,
     environment: 'jsdom',
